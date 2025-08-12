96a3ec3c1235b7bbecbba6ef123a02f10a6b5b09
UB2002 <udaybhaskar0532@gmail.com>
2025-08-12 15:19:39 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ io.on('connection', socket => {
 });
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
     console.error('MongoDB connect error:', err.message);
